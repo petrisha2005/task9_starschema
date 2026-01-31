7c88dbabe81494bd10443202b47eb9921689fcc2
patricia  V <patriciav@Patricia-mac-book.local>
2026-01-31 19:30:23 +0530

    main

---
+++ task9_star_schema_output.sql
@@ -0,0 +1,53 @@
+COPY 51290
+CREATE TABLE
+CREATE TABLE
+CREATE TABLE
+CREATE TABLE
+INSERT 0 0
+INSERT 0 0
+INSERT 0 3819
+INSERT 0 0
+CREATE TABLE
+INSERT 0 307740
+CREATE INDEX
+CREATE INDEX
+CREATE INDEX
+CREATE INDEX
+     region     | total_sales 
+----------------+-------------
+ Africa         |     7053984
+ Canada         |      602388
+ Caribbean      |     2918529
+ Central        |    25401591
+ Central Asia   |     6775551
+ East           |     6109506
+ EMEA           |     7255656
+ North          |    11233728
+ North Asia     |     7635141
+ Oceania        |     9901863
+ South          |    14408640
+ Southeast Asia |     7959942
+ West           |     6529626
+(13 rows)
+
+    category     | total_profit  
+-----------------+---------------
+ Furniture       |  2566842.5142
+ Office Supplies |  4666264.5087
+ Technology      | 5974008.59862
+(3 rows)
+
+ count  
+--------
+ 102580
+(1 row)
+
+ count  
+--------
+ 461610
+(1 row)
+
+ sales_key | customer_key | product_key | location_key | date_key | sales | profit | quantity | discount | shipping_cost | record_count 
+-----------+--------------+-------------+--------------+----------+-------+--------+----------+----------+---------------+--------------
+(0 rows)
+
